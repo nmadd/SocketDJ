fd611e388cfeaa9fddb295b45a831c2b2ba0eb04
nmadd <nate@fratmusic.com>
2016-01-21 11:59:39 -0500

    Basic scaffolding

---
+++ start.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('node_modules'));
+app.use(express.static(__dirname + '/frontend'));
+
+
+app.get('/', function(req,res,next){
+	res.status(200).sendFile(__dirname + '/index.html');
+})
+
+
+app.listen(2727, function(){
+	console.log('Listening on port 2727');
+})
